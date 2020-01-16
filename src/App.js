a2c45a1902c2a4b5b5d25ba5ebbe72a5836cb7f8
Betzalel <silverbez@gmail.com>
2020-01-16 17:53:10 +0200

    store up

--- src/App.js
+++ src/App.js
@@ -2,26 +2,34 @@ import React, { Component } from 'react';
 import { observer } from 'mobx-react'
 import './App.css';
 import Item from './components/Item';
-
+import DevTools from 'mobx-react-devtools';
 
 @observer
 class App extends Component {
+
   handleChange = (e) => {
     this.setState({
       newItem: e.target.value
     })
   }
+
   addItem = () => {
-    this.props.store.addItem(this.state.newItem)
+    this.props.shopping.addItem(this.state.newItem)
   }
+
   render() {
     return (
       <div className="App">
+        <DevTools/>
         <input onChange = {this.handleChange}/>
         <button onClick = {this.addItem}>Add</button>
       {/* your code here
           You should map each grocery item into an Item component  
       */}  
+        {this.props.shopping.list.map((i,ind) => <Item item = {i} 
+                                                    key = {ind}
+                                                    shopping = {this.props.shopping}/>
+          )}
       </div>
     );
   }
